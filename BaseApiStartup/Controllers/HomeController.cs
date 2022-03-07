b91c52b13c7f29ed70f592f4f7ff177480050ca6
tonycruz-dev <tonycruz-dev@outlook.com>
2022-03-07 22:22:12 +0000

    Create Home Controller

---
+++ BaseApiStartup/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace BaseApiStartup.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => Ok("Hello Mario the pricess is in another castle!");
+    }
+}
